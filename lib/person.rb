a2a2634859cb23295c7505c28f04308b7f3d0e43
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-28 23:13:30 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
   #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(avi_attributes)
+    avi_attributes.map {|k, v| self.send("#{k}=", v)}
+    # @name = name
+    # @eye_color = eye_color
+    # @height = height
+  end
+
+  # avi_attributes = {name: "Avi", eye_color: "brown", height: "short"}
+
 end
